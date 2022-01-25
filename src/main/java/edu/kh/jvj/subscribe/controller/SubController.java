3a37925ff817313c0e23743fcaaa5c93e8dc0096
JooDB <joodb0126@gmail.com>
2022-01-25 14:07:47 +0900

    헤더 수정, subBread.jsp 추가, 이미지 폴더 변경

---
+++ src/main/java/edu/kh/jvj/subscribe/controller/SubController.java
@@ -0,0 +1,27 @@
+package edu.kh.jvj.subscribe.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/subscribe/")
+public class SubController {
+	
+	
+	// 구독 메인 
+	@GetMapping("subMain")
+	public String subMain() {
+		return "/subscribe/subMain";
+	}
+
+	
+	// 빵 상세페이지
+	@GetMapping("subBread")
+	public String subBread() {
+		return "/subscribe/subBread";
+	}
+	
+
+}
